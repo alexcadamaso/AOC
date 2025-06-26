370844f4d6b88ee674c231dbbd8499a9865e9b97
cefet-5 <jeanjesuspedrobook@gmail.com>
2025-06-26 13:14:48 -0300

    ula 1 bit arrumado

--- simulation/qsim/work/@r@e@g@i@s@t@e@r_8bit_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity REGISTER_8bit_vlg_check_tst is
-    port(
-        OUTPUT          : in     vl_logic_vector(7 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end REGISTER_8bit_vlg_check_tst;
--- simulation/qsim/work/@r@e@g@i@s@t@e@r_8bit_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity REGISTER_8bit_vlg_vec_tst is
-end REGISTER_8bit_vlg_vec_tst;
