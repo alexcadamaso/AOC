777041976509f70c6088aeee767c874b7fc563c7
Jean Pedro <jeanjesuspedrobook@gmail.com>
2025-12-10 17:00:39 -0300

    mic1 final

--- simulation/qsim/work/@m@p@c_generator_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity MPC_generator_vlg_check_tst is
-    port(
-        MPC             : in     vl_logic_vector(8 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end MPC_generator_vlg_check_tst;
--- simulation/qsim/work/decoder_2x4/_primary.vhd
+++ simulation/qsim/work/decoder_2x4/_primary.vhd
@@ -2,11 +2,11 @@ library verilog;
 use verilog.vl_types.all;
 entity decoder_2x4 is
     port(
-        \00\            : out    vl_logic;
-        I1              : in     vl_logic;
+        O0              : out    vl_logic;
         I0              : in     vl_logic;
-        \01\            : out    vl_logic;
-        \02\            : out    vl_logic;
-        \03\            : out    vl_logic
+        I1              : in     vl_logic;
+        O1              : out    vl_logic;
+        O2              : out    vl_logic;
+        O3              : out    vl_logic
     );
 end decoder_2x4;
